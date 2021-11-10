849d73659f10fbf968c621c3505d926a5a834c91
Jeffrey Stines <jmstines00@gmail.com>
2021-11-09 22:26:59 -0500

    Hand update grid system dynamic working

--- black-jack-web/src/components/Hand.vue
+++ black-jack-web/src/components/Hand.vue
@@ -4,8 +4,7 @@
       v-for="card in cards"
       :key="card.order"
       :src="card.image"
-      :class="cardClass(card.order)"
-      :style="zIndex(card.order)"
+      :style="cardStyle(card.order)"
     />
   </v-container>
 </template>
@@ -24,25 +23,13 @@ export default Vue.extend({
   computed: {},
   data: () => ({}),
   methods: {
-    imageOffset: (order: number): string => {
-      return order != 0 ? "-4" : "0";
-    },
-    imageStyle: (order: number): string => {
-      const pixels = (order - 1) * 30;
-      let style = `left:${pixels}px;`;
-      if (order !== 0) {
-        style = `${style} position:absolute; margin-top:-200px;`;
-      } else {
-        style = `right:${100}px;`;
-      }
-      return style;
-    },
-    zIndex(order: number): string {
-      return `z-index: ${order * 10};`;
-    },
-    cardClass(order: number) {
-      return `card${order + 1}`;
-    },
+    cardStyle(order: number): string {
+      return `
+        z-index: ${order * 10};
+        grid-column: ${order + 1} / span 2;
+        grid-row: row 1
+        `;
+    }
   }
 });
 </script>
@@ -52,25 +39,5 @@ export default Vue.extend({
   display: grid;
   grid-gap: 0px;
   grid-template-columns: repeat(6, [col] 55px);
-}
-.card1 {
-  grid-column: col 1 / span 2;
-  grid-row: row 1;
-}
-.card2 {
-  grid-column: col 2 / span 2;
-  grid-row: row 1;
-}
-.card3 {
-  grid-column: col 3 / span 2;
-  grid-row: row 1;
-}
-.card4 {
-  grid-column: col 4 / span 2;
-  grid-row: row 1;
-}
-.card5 {
-  grid-column: col 5 / span 2;
-  grid-row: row 1;
 }
 </style>
